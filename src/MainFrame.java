3a52fbea2403c91e4b96239f85cfde49301c38a0
ZuzaTyszka <zuzia.tyszka@vp.pl>
2023-01-16 21:54:49 +0100

    okienko do wyboru pliku i dropdown do filtrowania

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -15,34 +15,64 @@ public class MainFrame extends JFrame {
         this.setTitle("Web Crawler");
         this.setLayout(null);
 
-        JLabel label = new JLabel("url:");
-        label.setFont(myFont);
-        label.setBounds(30, 25, 40, 40);
-        this.add(label);
+        JLabel urlLabel = new JLabel("url:");
+        urlLabel.setFont(myFont);
+        urlLabel.setBounds(30, 25, 100, 40);
+        this.add(urlLabel);
 
         JTextField urlField = new JTextField();
         urlField.setFont(myFont);
-        urlField.setBounds(70, 25, 590, 40);
+        urlField.setBounds(120, 25, 530, 40);
         this.add(urlField);
 
-        JButton button = new JButton("Crawl");
-        button.setFont(myFont);
-        button.setBounds(670, 25, 100, 39);
-        button.addActionListener(e -> {
+        JButton crawlButton = new JButton("Crawl");
+        crawlButton.setFont(myFont);
+        crawlButton.setBounds(670, 25, 100, 39);
+        crawlButton.addActionListener(e -> {
             try {
                 textArea.setText(readWebPage(urlField.getText()));
             } catch (IOException ex) {
-                textArea.setText("Nie można otworzyć strony o podanym aresie");
+                textArea.setText("Nie można otworzyć strony o podanym adresie");
                 throw new RuntimeException(ex);
             }
         });
-        this.add(button);
-        this.getRootPane().setDefaultButton(button);
+        this.add(crawlButton);
+        this.getRootPane().setDefaultButton(crawlButton);
+
+        JLabel saveToFileLabel = new JLabel("Save to file: ");
+        saveToFileLabel.setFont(myFont);
+        saveToFileLabel.setBounds(30, 75, 100, 39);
+        this.add(saveToFileLabel);
+
+        JButton saveToFileButton = new JButton("Choose file");
+        saveToFileButton.setFont(myFont);
+        saveToFileButton.setBounds(120, 75, 150, 39);
+        saveToFileButton.addActionListener(e -> {
+            try {
+                JFileChooser saveToFileChooser = new JFileChooser();
+                saveToFileChooser.showSaveDialog(null);
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        this.add(saveToFileButton);
+
+        JLabel filterByLabel = new JLabel("Filter by: ");
+        filterByLabel.setFont(myFont);
+        filterByLabel.setBounds(30, 125, 100, 40);
+        this.add(filterByLabel);
+
+
+        JComboBox filterComboBox = new JComboBox(new String[] {"Class name", "Id", "Type"});
+        filterComboBox.setBounds(120, 125, 100, 40);
+        this.add(filterComboBox);
+
+
 
         textArea.setFont(myFont);
         textArea.setEditable(false);
         JScrollPane scroll = new JScrollPane(textArea);
-        scroll.setBounds(20, 75, 750, 475);
+        scroll.setBounds(20, 175, 750, 475);
 //        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         this.add(scroll);
 

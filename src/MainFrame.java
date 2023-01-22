2055f2888cba00d51ae52f6d29d5d18b151a6604
ZuzaTyszka <zuzia.tyszka@vp.pl>
2023-01-22 22:35:20 +0100

    podstawowy layout

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,13 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URL;
 
 public class MainFrame extends JFrame {
     static Font myFont = new Font("Arial", Font.BOLD, 16);
     JTextArea textArea = new JTextArea();
+    File destionationFile;
 
     MainFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -22,7 +21,7 @@ public class MainFrame extends JFrame {
 
         JTextField urlField = new JTextField();
         urlField.setFont(myFont);
-        urlField.setBounds(120, 25, 530, 40);
+        urlField.setBounds(150, 25, 500, 40);
         this.add(urlField);
 
         JButton crawlButton = new JButton("Crawl");
@@ -46,11 +45,12 @@ public class MainFrame extends JFrame {
 
         JButton saveToFileButton = new JButton("Choose file");
         saveToFileButton.setFont(myFont);
-        saveToFileButton.setBounds(120, 75, 150, 39);
+        saveToFileButton.setBounds(150, 75, 120, 39);
         saveToFileButton.addActionListener(e -> {
             try {
                 JFileChooser saveToFileChooser = new JFileChooser();
                 saveToFileChooser.showSaveDialog(null);
+                destionationFile = saveToFileChooser.getSelectedFile();
             } catch (Exception ex) {
                 throw new RuntimeException(ex);
             }
@@ -59,20 +59,49 @@ public class MainFrame extends JFrame {
 
         JLabel filterByLabel = new JLabel("Filter by: ");
         filterByLabel.setFont(myFont);
-        filterByLabel.setBounds(30, 125, 100, 40);
+        filterByLabel.setBounds(30, 125, 120, 40);
         this.add(filterByLabel);
 
 
-        JComboBox filterComboBox = new JComboBox(new String[] {"Class name", "Id", "Type"});
-        filterComboBox.setBounds(120, 125, 100, 40);
+        JComboBox filterComboBox = new JComboBox(new String[] {"None", "Class name", "Id", "Type"});
+        filterComboBox.setBounds(150, 125, 120, 40);
+        filterComboBox.setFont(myFont);
         this.add(filterComboBox);
 
+        JCheckBox rememberMyChoiceCheckbox = new JCheckBox();
+        rememberMyChoiceCheckbox.setText("Remember my choice");
+        rememberMyChoiceCheckbox.setBounds(30, 175, 150, 40);
+        this.add(rememberMyChoiceCheckbox);
+
+        JButton extractButton = new JButton("Extract");
+        extractButton.setFont(myFont);
+        extractButton.setBounds(650, 175, 120, 39);
+        extractButton.addActionListener(e -> {
+            try {
+                if (destionationFile == null)
+                {
+                    JDialog errorDialog = new JDialog();
+                    errorDialog.setLocationRelativeTo(this);
+                    Container errorDialogContent = new JLabel("There's no file selected!");
+                    errorDialog.setContentPane(errorDialogContent);
+                    errorDialog.setSize(200, 100);
+
+
+                    errorDialog.setVisible(true);
+
+                }
+                else extractToFile();
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        this.add(extractButton);
 
 
         textArea.setFont(myFont);
         textArea.setEditable(false);
         JScrollPane scroll = new JScrollPane(textArea);
-        scroll.setBounds(20, 175, 750, 475);
+        scroll.setBounds(20, 225, 750, 475);
 //        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         this.add(scroll);
 
@@ -98,5 +127,9 @@ public class MainFrame extends JFrame {
 
             return sb.toString();
         }
+    }
+
+    public static void extractToFile(){
+
     }
 }

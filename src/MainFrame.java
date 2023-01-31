cee8a61b97d128d2fc6c4de04efefd3e083eaa55
kowalakk <konradkowalak@gmail.com>
2023-01-31 11:19:28 +0100

    extracting

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,5 +1,7 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import javax.swing.*;
 import java.awt.*;
@@ -7,26 +9,29 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.IntStream;
 
 public class MainFrame extends JFrame {
     static Font myFont = new Font("Arial", Font.BOLD, 16);
     JTextArea textArea = new JTextArea();
     File destinationFile;
     String settingsFilePath = "src/settings.txt";
+    Document document;
     JTextField filterTextBox;
     String[] parseFilters = new String[]{"None", "Class name", "Id", "Type"};
     int selectedFilter;
     JTextField urlField;
     JComboBox<String> filterComboBox;
+    JCheckBox chooseTextCheckbox;
+    JTextField chooseAttributeTextBox;
     JLabel saveToFileFileNameLabel;
 
     MainFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(800, 800);
+        this.setSize(800, 1000);
         this.setTitle("Web Crawler");
         this.setLayout(null);
 
+        // controls regarding reading url
         JLabel urlLabel = new JLabel("url:");
         urlLabel.setFont(myFont);
         urlLabel.setBounds(30, 25, 100, 40);
@@ -42,7 +47,8 @@ public class MainFrame extends JFrame {
         crawlButton.setBounds(670, 25, 100, 39);
         crawlButton.addActionListener(e -> {
             try {
-                textArea.setText(parseDocument(readWebPage(urlField.getText())));
+                document = readWebPage(urlField.getText());
+                textArea.setText(parseDocument().toString());
             } catch (IOException ex) {
                 textArea.setText("Requested address cannot be reached");
                 throw new RuntimeException(ex);
@@ -51,45 +57,14 @@ public class MainFrame extends JFrame {
         this.add(crawlButton);
         this.getRootPane().setDefaultButton(crawlButton);
 
-        JLabel saveToFileLabel = new JLabel("Save to file: ");
-        saveToFileLabel.setFont(myFont);
-        saveToFileLabel.setBounds(30, 75, 100, 39);
-        this.add(saveToFileLabel);
-
-        saveToFileFileNameLabel = new JLabel("");
-        saveToFileFileNameLabel.setBounds(300, 75, 120, 40);
-        this.add(saveToFileFileNameLabel);
-
-        JButton saveToFileButton = new JButton("Choose file");
-        saveToFileButton.setFont(myFont);
-        saveToFileButton.setBounds(150, 75, 120, 39);
-        saveToFileButton.addActionListener(e -> {
-            try {
-                JFileChooser saveToFileChooser = new JFileChooser();
-                saveToFileChooser.showSaveDialog(null);
-                destinationFile = saveToFileChooser.getSelectedFile();
-                saveToFileFileNameLabel.setText(destinationFile.getName());
-            } catch (Exception ex) {
-                throw new RuntimeException(ex);
-            }
-        });
-        this.add(saveToFileButton);
-
-
-
+        // controls regarding filtering data
         JLabel filterByLabel = new JLabel("Filter by: ");
         filterByLabel.setFont(myFont);
-        filterByLabel.setBounds(30, 125, 120, 40);
+        filterByLabel.setBounds(30, 75, 120, 40);
         this.add(filterByLabel);
 
-        filterTextBox = new JTextField();
-        filterTextBox.setBounds(300, 125, 120, 40);
-        filterTextBox.setFont(myFont);
-        filterTextBox.setVisible(false);
-        this.add(filterTextBox);
-
         filterComboBox = new JComboBox<>(parseFilters);
-        filterComboBox.setBounds(150, 125, 120, 40);
+        filterComboBox.setBounds(150, 75, 120, 40);
         filterComboBox.setFont(myFont);
         filterComboBox.addActionListener(e -> {
             selectedFilter = filterComboBox.getSelectedIndex();
@@ -97,18 +72,48 @@ public class MainFrame extends JFrame {
         });
         this.add(filterComboBox);
 
-        JCheckBox usePreviousSettingsCheckbox = new JCheckBox();
-        usePreviousSettingsCheckbox.setText("Use previous settings");
-        usePreviousSettingsCheckbox.setBounds(30, 175, 150, 40);
-        usePreviousSettingsCheckbox.addActionListener(e -> {
+        filterTextBox = new JTextField();
+        filterTextBox.setBounds(300, 75, 120, 40);
+        filterTextBox.setFont(myFont);
+        filterTextBox.setVisible(false);
+        this.add(filterTextBox);
+
+        JButton filterButton = new JButton("Filter");
+        filterButton.setFont(myFont);
+        filterButton.setBounds(650, 125, 120, 39);
+        filterButton.addActionListener(e -> {
             try {
-                if (usePreviousSettingsCheckbox.isSelected())
-                    getSettingsFromFile();
+                textArea.setText(parseDocument().toString());
             } catch (Exception ex) {
-                throw new RuntimeException(ex);
+                textArea.setText("There's no url selected!");
             }
         });
-        this.add(usePreviousSettingsCheckbox);
+        this.add(filterButton);
+
+        // controls regarding extracting data
+        JLabel chooseLabel = new JLabel("Choose: ");
+        chooseLabel.setFont(myFont);
+        chooseLabel.setBounds(30, 125, 120, 40);
+        this.add(chooseLabel);
+
+        chooseTextCheckbox = new JCheckBox();
+        chooseTextCheckbox.setText("Text");
+        chooseTextCheckbox.setFont(myFont);
+        chooseTextCheckbox.setBounds(150, 125, 100, 40);
+        this.add(chooseTextCheckbox);
+
+        JCheckBox chooseAttributeCheckbox = new JCheckBox();
+        chooseAttributeCheckbox.setText("Attribute");
+        chooseAttributeCheckbox.setFont(myFont);
+        chooseAttributeCheckbox.setBounds(250, 125, 100, 40);
+        chooseAttributeCheckbox.addActionListener(e -> chooseAttributeTextBox.setVisible(chooseAttributeCheckbox.isSelected()));
+        this.add(chooseAttributeCheckbox);
+
+        chooseAttributeTextBox = new JTextField();
+        chooseAttributeTextBox.setBounds(400, 125, 120, 40);
+        chooseAttributeTextBox.setFont(myFont);
+        chooseAttributeTextBox.setVisible(false);
+        this.add(chooseAttributeTextBox);
 
         JButton extractButton = new JButton("Extract");
         extractButton.setFont(myFont);
@@ -121,12 +126,12 @@ public class MainFrame extends JFrame {
                     Container errorDialogContent = new JLabel("There's no file selected!");
                     errorDialog.setContentPane(errorDialogContent);
                     errorDialog.setSize(200, 100);
-
-
                     errorDialog.setVisible(true);
-
                 } else {
-                    saveSettingsToFile(urlField.getText(), filterComboBox.getSelectedItem().toString(), filterTextBox.getText());
+                    saveSettingsToFile(
+                            urlField.getText(),
+                            Objects.requireNonNull(filterComboBox.getSelectedItem()).toString(),
+                            filterTextBox.getText());
                     extractToFile();
                 }
             } catch (Exception ex) {
@@ -135,12 +140,51 @@ public class MainFrame extends JFrame {
         });
         this.add(extractButton);
 
+        // checkbox regarding importing previous settings
+        JCheckBox usePreviousSettingsCheckbox = new JCheckBox();
+        usePreviousSettingsCheckbox.setText("Use previous settings");
+        usePreviousSettingsCheckbox.setFont(myFont);
+        usePreviousSettingsCheckbox.setBounds(30, 225, 250, 40);
+        usePreviousSettingsCheckbox.addActionListener(e -> {
+            try {
+                if (usePreviousSettingsCheckbox.isSelected())
+                    getSettingsFromFile();
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        this.add(usePreviousSettingsCheckbox);
 
+        // controls regarding saving data to file
+        JLabel saveToFileLabel = new JLabel("Save to file: ");
+        saveToFileLabel.setFont(myFont);
+        saveToFileLabel.setBounds(30, 175, 100, 39);
+        this.add(saveToFileLabel);
+
+        saveToFileFileNameLabel = new JLabel("");
+        saveToFileFileNameLabel.setBounds(300, 175, 120, 40);
+        this.add(saveToFileFileNameLabel);
+
+        JButton saveToFileButton = new JButton("Choose file");
+        saveToFileButton.setFont(myFont);
+        saveToFileButton.setBounds(150, 175, 120, 39);
+        saveToFileButton.addActionListener(e -> {
+            try {
+                JFileChooser saveToFileChooser = new JFileChooser();
+                saveToFileChooser.showSaveDialog(null);
+                destinationFile = saveToFileChooser.getSelectedFile();
+                saveToFileFileNameLabel.setText(destinationFile.getName());
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        this.add(saveToFileButton);
+
+        // area to show extracted data
         textArea.setFont(myFont);
         textArea.setEditable(false);
         JScrollPane scroll = new JScrollPane(textArea);
-        scroll.setBounds(20, 225, 750, 475);
-//        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scroll.setBounds(20, 275, 750, 475);
         this.add(scroll);
 
         this.setVisible(true);
@@ -159,29 +203,29 @@ public class MainFrame extends JFrame {
     }
 
     private void getSettingsFromFile() throws IOException {
-        try{
+        try {
             var br = new BufferedReader(new FileReader(settingsFilePath));
             String line;
-            while ((line = br.readLine()) != null){
+            while ((line = br.readLine()) != null) {
                 String[] args = line.split(" ");
-                switch (args[0]){
+                switch (args[0]) {
                     case "url":
                         urlField.setText(args[1]);
                         break;
                     case "file":
-                        String filename = args[1];
-                        for(int i = 2; i < args.length; i++){   // odtworzenie nazwy pliku zawierającej spacje
-                            filename += " ";
-                            filename += args[i];
+                        StringBuilder filename = new StringBuilder(args[1]);
+                        for (int i = 2; i < args.length; i++) {   // odtworzenie nazwy pliku zawierającej spacje
+                            filename.append(" ");
+                            filename.append(args[i]);
                         }
-                        destinationFile = new File(filename);
+                        destinationFile = new File(filename.toString());
                         saveToFileFileNameLabel.setText(destinationFile.getName());
                         break;
                     case "filter":
-                        filterComboBox.setSelectedIndex(new ArrayList<String>(List.of(parseFilters)).indexOf(args[1]));
+                        filterComboBox.setSelectedIndex(new ArrayList<>(List.of(parseFilters)).indexOf(args[1]));
                         break;
                     case "filter_string":
-                        if(args.length > 1)
+                        if (args.length > 1)
                             filterTextBox.setText(args[1]);
                         break;
                     default:
@@ -193,28 +237,51 @@ public class MainFrame extends JFrame {
         }
     }
 
-        private String parseDocument (Document document){
-            switch (selectedFilter) {
-                case 1:
-                    return Objects.requireNonNull(document.getElementsByClass(filterTextBox.getText())).toString();
-                case 2:
-                    return Objects.requireNonNull(document.getElementById(filterTextBox.getText())).toString();
-                case 3:  // Type
-                    return document.select(filterTextBox.getText()).toString();
-                default: // None
-                    return document.toString();
-            }
-        }
-
-        public static Document readWebPage (String urltext) throws IOException {
-
-            if (!urltext.startsWith("http") && !urltext.startsWith("file://"))
-                urltext = "https://" + urltext;
-
-            return Jsoup.connect(urltext).get();
-        }
-
-        public static void extractToFile () {
-
+    private Elements parseDocument() {
+        switch (selectedFilter) {
+            case 1:
+                return Objects.requireNonNull(document.getElementsByClass(filterTextBox.getText()));
+            case 2:
+                return Objects.requireNonNull(document.getElementById(filterTextBox.getText())).children();
+            case 3:  // Type
+                return document.select(filterTextBox.getText());
+            default: // None
+                return document.children();
         }
     }
+
+    public static Document readWebPage(String urltext) throws IOException {
+
+        if (urltext.startsWith("http"))
+            return Jsoup.connect(urltext).get();
+        if (urltext.startsWith("www")) {
+            urltext = "https://" + urltext;
+            return Jsoup.connect(urltext).get();
+        }
+        if (urltext.startsWith("file:///")) {
+            File input = new File(urltext.substring(8));
+            return Jsoup.parse(input, "UTF-8");
+        }
+        File input = new File(urltext);
+        return Jsoup.parse(input, "UTF-8");
+    }
+
+    public void extractToFile() {
+        try {
+            FileWriter fileWriter = new FileWriter(destinationFile);
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+
+            Elements elements = parseDocument();
+            for (Element element : elements) {
+                if (chooseTextCheckbox.isSelected())
+                    printWriter.printf("%s\n", element.text());
+                if (chooseAttributeTextBox.isVisible())
+                    printWriter.printf("%s = %s\n", chooseAttributeTextBox.getText(), element.attr(chooseAttributeTextBox.getText()));
+            }
+            printWriter.close();
+            fileWriter.close();
+        } catch (Exception ex) {
+            textArea.setText("There's no file selected!");
+        }
+    }
+}
